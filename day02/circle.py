f07f2b1fb7f88cb9a7cc64e5f2d2ba3e453c0cc8
Yoveledri <EDRIYOVEL9@GMAIL.COM>
2025-04-08 20:49:21 +0300

    Create circle.py

---
+++ day02/circle.py
@@ -0,0 +1,4 @@
+radius = float(input("Please enter desired circle radius: "))
+circumference = 2 * 3.14 * radius    
+area = 3.14 * radius * radius 
+print(f"Circle's circumference is {circumference:.2f} and area is {area:.2f}")
